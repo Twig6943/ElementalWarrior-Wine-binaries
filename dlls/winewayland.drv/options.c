dali546/wine

winewayland.drv: Experimental support for systray.
Use a standalone systray, adapted from winex11, to present icons for
applications.

Because the context menu on icons is drawn using global positioning
across process boundaries, we cannot currently anchor the menu window
properly to its effective parent (i.e., the icon), and we draw it as a
toplevel instead. Practically this has the effect that the menu window
will be placed at arbitrary locations on the screen by the Wayland
compositor.

Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

dlls/winewayland.drv/options.c
@@ -31,6 +31,11 @@
 #include "winreg.h"
 #include "winuser.h"
 
+#define IS_OPTION_TRUE(ch) \
+    ((ch) == 'y' || (ch) == 'Y' || (ch) == 't' || (ch) == 'T' || (ch) == '1')
+
+extern BOOL show_systray;
+
 /***********************************************************************
  *		get_config_key
  *
@@ -86,6 +91,9 @@ void wayland_read_options_from_registry(struct wayland *wayland)
         else if (!strcasecmp(buffer, "Compositor"))
             wayland->hidpi_scaling = WAYLAND_HIDPI_SCALING_COMPOSITOR;
     }
+
+    if (!get_config_key(hkey, appkey, "ShowSystray", RRF_RT_REG_SZ, buffer, sizeof(buffer)))
+        show_systray = IS_OPTION_TRUE(buffer[0]);
 
     if (appkey) RegCloseKey(appkey);
     if (hkey) RegCloseKey(hkey);

Twig6943/ElementalWarrior-Wine-binaries

dlls/winewayland.drv/options.c
@@ -43,6 +43,7 @@
 
 char *option_drm_device = NULL;
 enum wayland_hidpi_scaling option_hidpi_scaling = WAYLAND_HIDPI_SCALING_APPLICATION;
+BOOL option_show_systray = TRUE;
 BOOL option_use_system_cursors = TRUE;
 
 /***********************************************************************
@@ -103,6 +104,9 @@ void wayland_read_options_from_registry(void)
         else if (!strcasecmp(buffer, "Compositor"))
             option_hidpi_scaling = WAYLAND_HIDPI_SCALING_COMPOSITOR;
     }
+
+    if (!get_config_key(hkey, appkey, "ShowSystray", REG_SZ, buffer, sizeof(buffer)))
+        option_show_systray = IS_OPTION_TRUE(buffer[0]);
 
     if (!get_config_key(hkey, appkey, "UseSystemCursors", REG_SZ, buffer, sizeof(buffer)))
         option_use_system_cursors = IS_OPTION_TRUE(buffer[0]);
dali546/wine

winewayland.drv: Add support for native Wayland cursors.
Until now, we were using the cursors provided by Wine. Add native
Wayland cursor support, respecting the theme being used in the system.

The "UseSystemCursors" driver option controls whether to use the
native Wayland cursors (the default) or use the Wine provided
cursors.

Signed-off-by: Leandro Ribeiro <leandro.ribeiro@collabora.com>
Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

dlls/winewayland.drv/options.c
@@ -31,6 +31,15 @@
 #include "winreg.h"
 #include "winuser.h"
 
+#define IS_OPTION_TRUE(ch) \
+    ((ch) == 'y' || (ch) == 'Y' || (ch) == 't' || (ch) == 'T' || (ch) == '1')
+
+/***********************************************************************
+ *              Config options
+ */
+
+BOOL option_use_system_cursors = TRUE;
+
 /***********************************************************************
  *		get_config_key
  *
@@ -52,6 +61,7 @@ static inline DWORD get_config_key(HKEY defkey, HKEY appkey, const char *name,
 void wayland_read_options_from_registry(void)
 {
     static const WCHAR waylanddriverW[] = {'\\','W','a','y','l','a','n','d',' ','D','r','i','v','e','r',0};
+    char buffer[64];
     WCHAR bufferW[MAX_PATH + 16];
     HKEY hkey, appkey = 0;
     DWORD len;
@@ -77,6 +87,9 @@ void wayland_read_options_from_registry(void)
             RegCloseKey(tmpkey);
         }
     }
+
+    if (!get_config_key(hkey, appkey, "UseSystemCursors", RRF_RT_REG_SZ, buffer, sizeof(buffer)))
+        option_use_system_cursors = IS_OPTION_TRUE(buffer[0]);
 
     if (appkey) RegCloseKey(appkey);
     if (hkey) RegCloseKey(hkey);

Twig6943/ElementalWarrior-Wine-binaries

dlls/winewayland.drv/options.c
@@ -34,6 +34,15 @@
 
 #include <string.h>
 
+#define IS_OPTION_TRUE(ch) \
+    ((ch) == 'y' || (ch) == 'Y' || (ch) == 't' || (ch) == 'T' || (ch) == '1')
+
+/***********************************************************************
+ *              Config options
+ */
+
+BOOL option_use_system_cursors = TRUE;
+
 /***********************************************************************
  *		get_config_key
  *
@@ -55,6 +64,7 @@ static inline DWORD get_config_key(HKEY defkey, HKEY appkey, const char *name,
 void wayland_read_options_from_registry(void)
 {
     static const WCHAR waylanddriverW[] = {'\\','W','a','y','l','a','n','d',' ','D','r','i','v','e','r',0};
+    char buffer[64];
     HKEY hkey, appkey = 0;
     DWORD process_name_len;
 
@@ -80,6 +90,9 @@ void wayland_read_options_from_registry(void)
             }
         }
     }
+
+    if (!get_config_key(hkey, appkey, "UseSystemCursors", REG_SZ, buffer, sizeof(buffer)))
+        option_use_system_cursors = IS_OPTION_TRUE(buffer[0]);
 
     if (appkey) NtClose(appkey);
     if (hkey) NtClose(hkey);